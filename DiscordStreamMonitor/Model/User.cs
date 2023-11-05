b77526652609b283bbdb7f2ca4d13431862a4ec7
Muhammad Afiq <blackzafiqz@gmail.com>
2023-11-05 18:24:05 +0800

    Add project files.

---
+++ DiscordStreamMonitor/Model/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordStreamMonitor.Model
+{
+    public class User
+    {
+        public ulong Id { get; set; }
+        public string Name { get; set; }
+        public string Nickname { get; set; }
+        public DateTime StartedStream { get; set; }
+        public ulong MessageId { get; set; }
+    }
+}
---
+++ DiscordStreamMonitor/Program.cs
@@ -0,0 +1,67 @@
+﻿using Discord;
+using Discord.WebSocket;
+using DiscordStreamMonitor.Model;
+using System.Diagnostics.Metrics;
+
+namespace DiscordStreamMonitor
+{
+    internal class Program
+    {
+        private static DiscordSocketClient _client;
+        private static List<User> _users = new();
+        public static async Task Main()
+        {
+            var _config = new DiscordSocketConfig { MessageCacheSize = 100 };
+            _client = new DiscordSocketClient(_config);
+            await _client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("DiscordToken"));
+            await _client.StartAsync();
+
+            _client.UserVoiceStateUpdated += UserVoiceStateUpdated;
+            _client.Ready += async () =>
+            {
+                Console.WriteLine("Bot is connected!");
+                await _client.SetGameAsync("Watching Streams");
+            };
+
+            await Task.Delay(-1);
+        }
+
+        private static async Task UserVoiceStateUpdated(SocketUser socketUser, SocketVoiceState state1, SocketVoiceState state2)
+        {
+            if (state1.VoiceChannel.Name != Environment.GetEnvironmentVariable("ChannelName"))
+                return;
+            var user = _users.Where(x => x.Id == socketUser.Id).FirstOrDefault();
+            if (user == null)
+            {
+                var userVoice = _client.GetGuild(state1.VoiceChannel.Guild.Id).GetUser(socketUser.Id);
+                if (!userVoice.IsStreaming)
+                    return;
+                user = new User()
+                {
+                    Id = userVoice.Id,
+                    Name = userVoice.Username,
+                    Nickname = userVoice.Nickname,
+                    StartedStream = DateTime.Now
+                };
+
+                String text = $"{user.Name}({user.Nickname})\n" +
+                    $"Started stream: {DateTime.Now}";
+                var res = await state1.VoiceChannel.SendMessageAsync(text: text);
+                user.MessageId = res.Id;
+                _users.Add(user);
+            }
+            else
+            {
+                var duration = DateTime.Now - user.StartedStream;
+                String text = $"{user.Name}({user.Nickname})\n" +
+                    $"Started stream: {user.StartedStream}\n" +
+                    $"Stopped stream: {DateTime.Now}\n" +
+                    $"Duration: {duration.ToString(@"h\h\ m\m\ s\s")}";
+
+                await state1.VoiceChannel.ModifyMessageAsync(user.MessageId, x => x.Content = text);
+                _users.Remove(user);
+            }
+        }
+
+    }
+}
